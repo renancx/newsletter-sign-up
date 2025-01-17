35ad95ed88d62bde96b8ac437f788ff8dcfe638d
renancx <renan.lowe@hotmail.com>
2025-01-17 13:41:52 -0300

    add primereact dependency and update newsletter signup UI

--- newsletter-signup/src/App.tsx
+++ newsletter-signup/src/App.tsx
@@ -1,28 +1,39 @@
 import './App.css'
+import { Dialog } from 'primereact/dialog';
+import { useState } from 'react';
+import iconSucess from '../assets/images/icon-success.svg';
+import iconList from '../assets/images/icon-list.svg';
 
 function App() {
+	const [visible, setVisible] = useState(false);
+
 	return (
 		<>
-			Stay updated!
+			<h1>Stay updated!</h1>
 
-			Join 60,000+ product managers receiving monthly updates on:
+			<p>Join 60,000+ product managers receiving monthly updates on:</p>
 
-			Product discovery and building what matters
-			Measuring to ensure updates are a success
-			And much more!
+			<ul>
+				<li><img src={iconList}/> Product discovery and building what matters</li>
+				<li><img src={iconList}/> Measuring to ensure updates are a success</li>
+				<li><img src={iconList}/> And much more!</li>
+			</ul>
 
-			Email address
-			email@company.com
+			<div>
+				<label htmlFor="email">Email address</label>
+				<input type="email" id="email" name="email" />
 
-			Subscribe to monthly newsletter
+				<button onClick={() => setVisible(true)}>Subscribe to monthly newsletter</button>
+			</div>
 
-
-			Thanks for subscribing!
-
-			A confirmation email has been sent to ash@loremcompany.com. 
-			Please open it and click the button inside to confirm your subscription.
-
-			Dismiss message
+			<Dialog header={<img src={iconSucess} alt="Success" />} visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
+				<h1>Thanks for subscribing!</h1>
+				<p>
+					A confirmation email has been sent to ash@loremcompany.com. 
+					Please open it and click the button inside to confirm your subscription.
+				</p>
+				<button onClick={() => setVisible(false)}>Dismiss message</button>
+			</Dialog>					
 		</>
 	)
 }
