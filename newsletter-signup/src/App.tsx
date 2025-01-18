1a7186eb009353b4b807574c4348fe942d9df423
Renan <renan.lowe@hotmail.com>
2025-01-18 15:17:39 -0300

    update styles and layout adjustments

--- newsletter-signup/src/App.tsx
+++ newsletter-signup/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 				<div className="content">
 					<h1 className="title">Stay updated!</h1>
 
-					<p className="">Join 60,000+ product managers receiving monthly updates on:</p>
+					<p className="text">Join 60,000+ product managers receiving monthly updates on:</p>
 
 					<ul className="ul">
 						<li className="li"><img src={iconList}/> Product discovery and building what matters</li>
@@ -23,10 +23,10 @@ function App() {
 					</ul>
 
 					<div className="form">
-						<label className="" htmlFor="email">Email address</label>
-						<input className="" type="email" id="email" name="email" />
+						<label className="email-title" htmlFor="email">Email address</label>
+						<input placeholder="email@company.com" className="email-input" type="email" id="email" name="email" />
 
-						<button className="" onClick={() => setVisible(true)}>Subscribe to monthly newsletter</button>
+						<button className="btn" onClick={() => setVisible(true)}>Subscribe to monthly newsletter</button>
 					</div>
 				</div>
 				<img className="img" src={illustration} />
@@ -34,11 +34,11 @@ function App() {
 
 			<Dialog className="dialog" header={<img src={iconSucess} alt="Success" />} dismissableMask={true} visible={visible} onHide={() => setVisible(false)}>
 				<h1 className="title">Thanks for subscribing!</h1>
-				<p className="">
+				<p className="text">
 					A confirmation email has been sent to ash@loremcompany.com. 
 					Please open it and click the button inside to confirm your subscription.
 				</p>
-				<button className="" onClick={() => setVisible(false)}>Dismiss message</button>
+				<button className="btn" onClick={() => setVisible(false)}>Dismiss message</button>
 			</Dialog>					
 		</>
 	)
